2fe0f1b9e69f016b8079beb4aec988fd97d6449b
Silver <122754864+Silverqq@users.noreply.github.com>
2023-05-11 07:21:37 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Director director = new Director("Иванов Иван Иванович", 1970, 500000);
-        DepartmentManager departmentManager = new DepartmentManager("Петров Петр Петрович", 1980, 150000);
-        Worker worker = new Worker("Сидоров Сидор Сидорович", 1990, 50000);
+        Director director = new Director("Ходяков Петр Александрович", 1970, 500000);
+        DepartmentManager departmentManager = new DepartmentManager("Корн Вячеслав Михайлович", 1980, 150000);
+        Worker worker = new Worker("Пресняков Геннадий Витальевич", 1990, 50000);
 
         director.info();
         System.out.println();
